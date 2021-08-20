cab3452a460a906179ef0732829925fba3c2d715
MatsushimaHirotada <t170d516@gunma-u.ac.jp>
2021-08-20 11:32:20 +0900

    複数カウンター実装

--- svelte-typescript-app/src/App.svelte
+++ svelte-typescript-app/src/App.svelte
@@ -1,30 +1,80 @@
 <script lang="ts">
-	export let name: string;
+
+    import Counter from './Counter.svelte';
+	import Box from './Box.svelte';
+
+	$:CounterArray = [{id:0,name:'new',count:0}];
+	$:Sum = 0;
+
+	function addCounter():void{
+		CounterArray[CounterArray.length] = {id:CounterArray.length,name:'new',count:0}
+		Sum=SumofCount()
+	}
+
+
+	function deleteCounter(event):void{
+		const id = event.detail.order
+		const firsthalfofArray = CounterArray.slice(0,id)
+		const letterhalfofArray = CounterArray.slice(id+1)
+		
+		for (const entry of letterhalfofArray){
+			firsthalfofArray[firsthalfofArray.length] = {id:firsthalfofArray.length,name:entry.name,count:entry.count};
+		}
+
+		CounterArray = firsthalfofArray;
+		Sum=SumofCount();
+
+	}
+
+	function updateCount(event){
+		const id = event.detail.Id;
+		const count = event.detail.Count;
+		
+		CounterArray[id] = {id:id,name:CounterArray[id].name,count:count};
+		Sum = SumofCount();
+
+	}
+
+	function SumofCount(){
+		let sum=0;
+
+		for(const entry of CounterArray){
+			sum = sum + entry.count;
+		}
+
+		return sum;
+	}
+
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<!-- svelte-ignore non-top-level-reactive-declaration -->
 
+<h1>カウンター</h1>
+
+<button on:click={addCounter}>
+	新しいカウンターを作成
+</button>
+
+{#each CounterArray as counter(counter.id)}
+	<Box>
+		<input bind:value={counter.name}>
+		{counter.count}
+		<Counter on:deleteorder={deleteCounter}
+		on:updatecountorder={updateCount}
+		id = {counter.id}
+		/>
+	</Box>
+{/each}
+
+	<div>title list:
+
+		{#each CounterArray as {name}}
+			{name},
+		{/each}
+	</div>
+
+	<div>
+		sum of count:{Sum}
+	</div>
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
